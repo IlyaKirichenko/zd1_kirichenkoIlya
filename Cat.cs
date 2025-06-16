309a1288f6f9945e5587b7e78dbe04634a836d58
IlyaKirichenko <ikirichenko345@gmail.com>
2025-06-16 10:45:37 +0500

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace zd1_KirchenkoIlya
+{
+    class Cat
+    { 
+        // Приватные поля для хранения данных
+        private string name;
+        private double weight;
+
+        // Свойство для имени с проверкой
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                // Проверка что имя состоит только из букв
+                bool OnlyLetters = true;
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+
+        // Свойство для веса с проверкой
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                // Вес должен быть больше 0
+                if (value > 0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - некорректный вес!");
+                }
+            }
+        }
+
+        // Конструктор класса
+        public Cat(string CatName, double СatWeight)
+        {
+            Name = CatName;
+            Weight = СatWeight;
+        }
+
+        // Метод для "мяуканья" кота
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} (вес: {Weight} кг): МЯЯЯУ!!!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd1_KirchenkoIlya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем первого кота
+            Console.WriteLine("Введите данные для первого кота:");
+            Cat cat1 = CreateCatFromInput();
+            // Создаем второго кота
+            Console.WriteLine("\nВведите данные для второго кота:");
+            Cat cat2 = CreateCatFromInput();
+
+            // Выводим информации
+            Console.WriteLine("\nРезультат:");
+            cat1.Meow();
+            cat2.Meow();
+            Console.ReadKey();
+        }
+
+        // Метод для создания кота с вводом данных
+        static Cat CreateCatFromInput()
+        {
+            Console.Write("Введите имя кота: ");
+            string name = Console.ReadLine();
+
+            double weight;
+            // Цикл для корректного ввода веса
+            while (true)
+            {
+                Console.Write("Введите вес кота (кг): ");
+                if (double.TryParse(Console.ReadLine(), out weight))
+                {
+                    if (weight >= 1 && weight <= 20)
+                    {
+                        break; // Выход при правильном вводе
+                    }
+                    
+                }
+                Console.WriteLine("Некорректный ввод веса! Попробуйте еще раз.");
+            }
+
+            return new Cat(name, weight);
+        }
+    }
+}
